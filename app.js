7fb7095dda7a48688809d9d879acfa260a82508f
Vahe <vahe.aslanyan@gmail.com>
2019-10-08 02:09:17 +0000

    add partials

--- app.js
+++ app.js
@@ -20,10 +20,21 @@ mongoose.connect(url, {
 });
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", function(req, res){
 	res.render("index")
+});
+
+app.get("/map", function(req, res){
+	res.redirect("/")
 })
+
+//new route
+app.get("/map/new", function(req, res){
+	res.render("new")
+});
 
 app.listen(3000, function(){
 	console.log("Server listening on port 3000")
