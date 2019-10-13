d7ee56b1c0db6798d153f82d8a123ddad49b696e
Vahe <vahe.aslanyan@gmail.com>
2019-10-13 22:42:15 +0000

    add user model, connect-flash, configure passport

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express 	= require("express"),
 	app			= express();
 
-var mongoose 	= require("mongoose");
-var bodyParser	= require("body-parser");
+var mongoose 	= require("mongoose"),
+	bodyParser	= require("body-parser"),
+	flash 		= require("connect-flash");
 
-var Location	= require("./models/location");
+var Location	= require("./models/location"),
+	User 		= require("./models/user");
 
 //an enviromental url for the db and backup for our mongodb url in case the enviromental variable breaks for some reason
 var url = process.env.DATABASEURL || "mongodb://localhost/travelmap";
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(flash());
 
 //mongoose setup	
 mongoose.set('useNewUrlParser', true);
@@ -22,9 +29,26 @@ mongoose.connect(url, {
 	console.log("ERROR: ", err.message);
 })
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "I never wear v-neck cuz I'm all about my crew, reppin westside yeah that W",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//passing the currentUser info, and flash messages to every route
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
 
 
 app.get("/", function(req, res){
